012e2570374053ad8a33b3ccc103faa5e52e2931
novyantsp <43920175+novyantsp@users.noreply.github.com>
2020-05-23 10:28:02 +0700

    Lenop: Add Model

--- app/Form.php
+++ app/Form.php
@@ -6,5 +6,23 @@ use Illuminate\Database\Eloquent\Model;
 
 class Form extends Model
 {
+    public function pasien()
+    {
+        return $this->hasMany('App\User');
+    }
 
+    public function anamnesis()
+    {
+        return $this->hasOne('App\Anamnesis');
+    }
+
+    public function pemerikasaan_fisik()
+    {
+        return $this->hasOne('App\PemeriksaanFisik');
+    }
+
+    public function disposisi()
+    {
+        return $this->hasOne('App\Disposisi');
+    }
 }
