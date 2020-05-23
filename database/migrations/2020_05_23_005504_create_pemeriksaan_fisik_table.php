bcafbb7d1c7a1b0e155be571dc4b8b4d23508394
novyantsp <43920175+novyantsp@users.noreply.github.com>
2020-05-23 09:24:57 +0700

    fix foreign key

--- database/migrations/2020_05_23_005504_create_pemeriksaan_fisik_table.php
+++ database/migrations/2020_05_23_005504_create_pemeriksaan_fisik_table.php
@@ -15,16 +15,24 @@ class CreatePemeriksaanFisikTable extends Migration
     {
         Schema::create('pemeriksaan_fisik', function (Blueprint $table) {
             $table->id();
+            $table->integer('mata_id')->unsigned();
             $table->foreign('mata_id')->references('id')->on('mata')->onDelete('cascade');
+            $table->integer('tht_id')->unsigned();
             $table->foreign('tht_id')->references('id')->on('tht')->onDelete('cascade');
+            $table->integer('leher_id')->unsigned();
             $table->foreign('leher_id')->references('id')->on('leher')->onDelete('cascade');
+            $table->integer('torak_id')->unsigned();
             $table->foreign('torak_id')->references('id')->on('torak')->onDelete('cascade');
+            $table->integer('jantung_id')->unsigned();
             $table->foreign('jantung_id')->references('id')->on('jantung')->onDelete('cascade');
+            $table->integer('paru_id')->unsigned();
             $table->foreign('paru_id')->references('id')->on('paru')->onDelete('cascade');
+            $table->integer('abdomen_id')->unsigned();
             $table->foreign('abdomen_id')->references('id')->on('abdomen')->onDelete('cascade');
             $table->string('hepar')->default('none');
             $table->string('lien')->default('none');
             $table->string('lain-lain')->default('none');
+            $table->integer('extremitas_id')->unsigned();
             $table->foreign('extremitas_id')->references('id')->on('extrimitas')->onDelete('cascade');
             $table->timestamps();
         });
